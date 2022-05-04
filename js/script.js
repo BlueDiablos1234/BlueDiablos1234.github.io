72044c1a92f29bc2bc1f7cce4b8b4802a6d50a62
Nathan <natengel35@gmail.com>
2022-05-04 19:43:23 +0200

    Fixed Incorrect Displaying of Dates on Enhanced PAS

--- js/script.js
+++ js/script.js
@@ -22,16 +22,16 @@ const eventData = {
     ["WEDE6011", "Part 1", "May 09, 2022, 21:00:00"],         
     ["ISEC6311", "Assignment", "May 12, 2022, 21:00:00"],
 
-    ["ISEC6311", "Take Home Test", "May 27, 2022, 21:00:00"], 
     ["ADDB7311", "On Campus Test", "May 17, 2022, 11:00:00"],
-    ["WEDE6011", "Part 2", "Jun 14, 2022, 21:00:00"],
+    ["ISEC6311", "Take Home Test", "May 27, 2022, 21:00:00"], 
+    ["OPSC7311", "Part 2", "Jun 03, 2022, 21:00:00"],
 
     ["ADDB7311", "Assignment", "Jun 07, 2022, 11:00:00"],
-    ["OPSC7311", "Part 2", "Jun 03, 2022, 21:00:00"],
+    ["WEDE6011", "Part 2", "Jun 14, 2022, 21:00:00"],   
+    ["ADDB7311", "On Campus Exam", "Jun 24, 2022, 08:00:00"], 
+               
+    ["OPSC7311", "POE", "Jun 29, 2022, 21:00:00"],
     ["ISEC7311", "Take Home Exam", "Jul 07, 2022, 21:00:00"],
-                
-    ["ADDB7311", "On Campus Exam", "Jun 24, 2022, 08:00:00"],            
-    ["OPSC7311", "POE", "Jun 29, 2022, 21:00:00"],  
     ["WEDE6011", "POE", "Jul 04, 2022, 21:00:00"]
   ]
 };
