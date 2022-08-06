e63a2d5c53c86e0a9b634e22164a0058f7990bb4
Nathan <ST10118723@vcconnect.edu.za>
2022-08-06 22:53:22 +0200

    Updated unindexed pages
    Also fixed 404 page error

--- js/note.js
+++ js/note.js
@@ -1,1 +1,28 @@
+let txtTitle = document.getElementById("txtTitle").addEventListener("input", input);
+let txtDesc = document.getElementById("txtDesc").addEventListener("input", input)
+const btnAdd = document.getElementById("btnAdd").addEventListener("click", submit);
+const btnLoad = document.getElementById("btnLoad").addEventListener("click", load);
+
+let title;
+let desc;
+function input() {
+  title = document.getElementById("txtTitle").value;
+  desc = document.getElementById("txtDesc").value;
+}
+
+function submit() {
+  localStorage.setItem("title", title);
+  localStorage.setItem("desc", desc);
+  console.log("Added!");
+}
+
+function load() {
+  document.getElementById("txtTitle").value = localStorage.getItem("title", "Default value")
+  document.getElementById("txtDesc").value = localStorage.getItem("desc", "Default value")
+  console.log("Loaded!");
+}
+
+
+
 localStorage.setItem("items", []);
+
