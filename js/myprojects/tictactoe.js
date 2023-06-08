d5e7b71f0e3bee850dc0a3c6e199663a986c02f9
Aiden Pearce <natengel35@gmail.com>
2023-06-08 22:22:20 +0200

    Polished tic-tac-toe Game

--- js/myprojects/tictactoe.js
+++ js/myprojects/tictactoe.js
@@ -9,31 +9,43 @@ const cell8 = document.getElementById('cell-8');
 const cell9 = document.getElementById('cell-9');
 const cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
 const body = document.getElementById('body');
+const Xscore = document.getElementById('x-score');
+const Oscore = document.getElementById('o-score');
+let x = 0;
+let o = 0;
+Xscore.innerHTML = "x = " + x;
+Oscore.innerHTML = "o = " + o;
 
 body.addEventListener(('contextmenu'), e => {
     e.preventDefault();
 });
 
 const play = (e) => {
+    if(e.target.innerText==='x' || e.target.innerText==='o') {
+        return;
+    }
     if(e.type === 'contextmenu') {
         e.target.innerText='x'
     } else {
         e.target.innerHTML='o'
     }
     calc();
-
 }
 
 const calc = () => {
     if(cell1.innerHTML==="x" && cell2.innerHTML==="x" && cell3.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell1.innerHTML==="o" && cell2.innerHTML==="o" && cell3.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -41,12 +53,16 @@ const calc = () => {
     if(cell4.innerHTML==="x" && cell5.innerHTML==="x" && cell6.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell4.innerHTML==="o" && cell5.innerHTML==="o" && cell6.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -54,12 +70,16 @@ const calc = () => {
     if(cell7.innerHTML==="x" && cell8.innerHTML==="x" && cell9.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell7.innerHTML==="o" && cell8.innerHTML==="o" && cell9.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -67,12 +87,16 @@ const calc = () => {
     if(cell1.innerHTML==="x" && cell4.innerHTML==="x" && cell7.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell1.innerHTML==="o" && cell4.innerHTML==="o" && cell7.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -80,12 +104,16 @@ const calc = () => {
     if(cell2.innerHTML==="x" && cell5.innerHTML==="x" && cell8.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell2.innerHTML==="o" && cell5.innerHTML==="o" && cell8.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -93,12 +121,16 @@ const calc = () => {
     if(cell3.innerHTML==="x" && cell6.innerHTML==="x" && cell9.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell3.innerHTML==="o" && cell6.innerHTML==="o" && cell9.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -106,12 +138,16 @@ const calc = () => {
     if(cell1.innerHTML==="x" && cell5.innerHTML==="x" && cell9.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell1.innerHTML==="o" && cell5.innerHTML==="o" && cell9.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
@@ -119,18 +155,34 @@ const calc = () => {
     if(cell3.innerHTML==="x" && cell5.innerHTML==="x" && cell7.innerHTML==="x") {
         setTimeout(() => {
             alert("x WINS")
+            x+=1;
+            Xscore.innerHTML = "x = " + x;
             clearBoard()
         }, 300);
     }
     if(cell3.innerHTML==="o" && cell5.innerHTML==="o" && cell7.innerHTML==="o") {
         setTimeout(() => {
             alert("o WINS")
+            o+=1;
+            Oscore.innerHTML = "o = " + o;
             clearBoard()
         }, 300);
     }
 }
 
 const clearBoard = () => {
+    if(x > o) {
+        Xscore.classList.add("text-sky-500")
+    }
+    if(o > x) {
+        Oscore.classList.add("text-sky-500")
+    }
+    if(x === o) {
+        Xscore.classList.remove("text-sky-500")
+    }
+    if(o === x) {
+        Oscore.classList.remove("text-sky-500")
+    }
     cells.map((item) => {
         item.innerText = ""
     })
